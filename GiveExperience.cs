667b57fe8cccacda8268999b609f6c763e248fab
victorsvensson <zeqqan@gmail.com>
2023-07-08 14:22:24 +0200

    Removed collider functions
    Removed some OnCollisionEnter functions since it needs rigidbody on the same object. Then it will not fulfill the functionality since for example Boxes shouldn't need to have a trigger. Will advice to use Is Trigger on the collider gameobject instead.

--- GiveExperience.cs
+++ GiveExperience.cs
@@ -12,12 +12,12 @@ namespace ZeqDEV
 
         [SerializeField] private bool _GiveExperienceOnDeath;
 
-        [Header("Make sure to add a Collider on this object using this option")]
+        [Header("Make sure to add a Collider with Is Trigger active on this object using this option")]
         [Tooltip("Gives experience when hitting a collider. You can use collision or Is Trigger")]
-        [SerializeField] private bool _GiveExperienceOnInteraction;
+        [SerializeField] private bool _giveExperienceOnColliderTrigger;
 
         [Space(10)]
-        [SerializeField] private int _experience; // Amount of experience that should be given to player
+        [SerializeField] private int _experience; // Amount of experience that should be given to player        
 
         private void Start()
         {
@@ -29,30 +29,37 @@ namespace ZeqDEV
             }
         }
 
+        // Gives experience when the gameobject is destroyed
         private void OnDestroy()
         {
             if (_GiveExperienceOnDeath) experienceManager.AddExperience(_experience);
         }
 
+        // Gives experience if this gameobject has 2D Collider component attached with Is Trigger active
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (_GiveExperienceOnInteraction) experienceManager.AddExperience(_experience);
+            if (_giveExperienceOnColliderTrigger) experienceManager.AddExperience(_experience);
         }
 
+        // Gives experience if this gameobject has a Collider component attached with Is Trigger active
         private void OnTriggerEnter(Collider other)
         {
-            if (_GiveExperienceOnInteraction) experienceManager.AddExperience(_experience);
+            if (_giveExperienceOnColliderTrigger) experienceManager.AddExperience(_experience);
         }
 
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (_GiveExperienceOnInteraction) experienceManager.AddExperience(_experience);
-        }
+        //// Gives experience if the gameobject has a Collider component attached with Is Trigger disabled
+        //private void OnCollisionEnter(Collision collision)
+        //{
+        //    if (_giveExperienceOnCollision) experienceManager.AddExperience(_experience);
+        //}
 
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (_GiveExperienceOnInteraction) experienceManager.AddExperience(_experience);
-        }
+        //// Gives experience if the gameobject has a 2D Collider component attached with Is Trigger disabled
+        //private void OnCollisionEnter2D(Collision2D collision)
+        //{
+        //    if (_giveExperienceOnCollision) experienceManager.AddExperience(_experience);
+        //}
+
+        
     }
 }
 
